8ddca48b30d514af459f458069563b072f360f9c
Alan_Gutierrez <ala.gutierrezv@duocuc.cl>
2025-10-14 19:07:10 -0300

    app
    cambios en app para los links y rutas

--- MilSaboresReact/src/App.jsx
+++ MilSaboresReact/src/App.jsx
@@ -1,14 +1,25 @@
-import Navbar from './pages/Navbar'
-import Footer from './components/Footer'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Productos from './pages/Productos';
+import Detalle from './pages/Detalle';
+import Carrito from './pages/Carrito';
+import Configuración from './pages/Configuración';
+import Contacto from './pages/Contacto';
+import Noticias from './pages/Noticias';
+import Pago from './pages/Pago';
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      
-      <Footer />
-    </>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Productos />} />
+      <Route path="/detalle/:codigo" element={<Detalle />} />
+      <Route path="/carrito" element={<Carrito />} />
+      <Route path="/configuracion" element={<Configuración />} />
+      <Route path="/contacto" element={<Contacto />} />
+      <Route path="/noticias" element={<Noticias />} />
+      <Route path="/pago" element={<Pago />} />
+    </Routes>
+  </BrowserRouter>
+);
 
-export default App
+export default App;
