91ba7ba5f35096940019d2e31df5b6679e22788d
Gubier Zamora <gub.zamora@duocuc.cl>
2025-10-14 19:17:19 -0300

    Pagina principal navbar y footer

--- MilSaboresReact/src/App.jsx
+++ MilSaboresReact/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './pages/Navbar';
+import Footer from './components/Footer';
 import Productos from './pages/Productos';
 import Detalle from './pages/Detalle';
 import Carrito from './pages/Carrito';
@@ -9,17 +11,21 @@ import Noticias from './pages/Noticias';
 import Pago from './pages/Pago';
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Productos />} />
-      <Route path="/detalle/:codigo" element={<Detalle />} />
-      <Route path="/carrito" element={<Carrito />} />
-      <Route path="/configuracion" element={<Configuración />} />
-      <Route path="/contacto" element={<Contacto />} />
-      <Route path="/noticias" element={<Noticias />} />
-      <Route path="/pago" element={<Pago />} />
-    </Routes>
-  </BrowserRouter>
+  <>
+    <Navbar />
+    <main style={{ minHeight: '70vh' }}>
+      <Routes>
+        <Route path="/" element={<Productos />} />
+        <Route path="/detalle/:codigo" element={<Detalle />} />
+        <Route path="/carrito" element={<Carrito />} />
+        <Route path="/configuracion" element={<Configuración />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Route path="/noticias" element={<Noticias />} />
+        <Route path="/pago" element={<Pago />} />
+      </Routes>
+    </main>
+    <Footer />
+  </>
 );
 
 export default App;
