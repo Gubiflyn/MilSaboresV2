f7882c032fe49b09a6bcc8c5e1c1ca7be47dfcfe
Gubier Zamora <gub.zamora@duocuc.cl>
2025-10-15 21:21:26 -0300

    inicio e css
    Hice la pagina de inicio y tambien agregue el css de nuestro proyecyo anterior

--- MilSaboresReact/src/App.jsx
+++ MilSaboresReact/src/App.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './pages/Navbar';
-import Footer from './components/Footer';
-import Productos from './pages/Productos';
-import Detalle from './pages/Detalle';
-import Carrito from './pages/Carrito';
-import Configuración from './pages/Configuración';
-import Contacto from './pages/Contacto';
-import Noticias from './pages/Noticias';
-import Pago from './pages/Pago';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './pages/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Productos from './pages/Productos'
+import Detalle from './pages/Detalle'
+import Carrito from './pages/Carrito'
+import Configuración from './pages/Configuración'
+import Contacto from './pages/Contacto'
+import Noticias from './pages/Noticias'
+import Pago from './pages/Pago'
+
 
 const App = () => (
   <>
     <Navbar />
     <main style={{ minHeight: '70vh' }}>
       <Routes>
-        <Route path="/" element={<Productos />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/productos" element={<Productos />} />
         <Route path="/detalle/:codigo" element={<Detalle />} />
         <Route path="/carrito" element={<Carrito />} />
         <Route path="/configuracion" element={<Configuración />} />
@@ -26,6 +29,6 @@ const App = () => (
     </main>
     <Footer />
   </>
-);
+)
 
 export default App;
