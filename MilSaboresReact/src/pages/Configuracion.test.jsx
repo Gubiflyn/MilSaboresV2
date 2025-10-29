a703c38faafb2b326bb20667889d43f80f6729d9
Gubier Zamora <gub.zamora@duocuc.cl>
2025-10-28 22:49:43 -0300

    Update Configuracion.test.jsx

--- MilSaboresReact/src/pages/Configuracion.test.jsx
+++ MilSaboresReact/src/pages/Configuracion.test.jsx
@@ -5,12 +5,22 @@ import "@testing-library/jest-dom";
 import Configuracion from "./Configuracion";
 
 describe("Configuracion (básico)", () => {
+<<<<<<< HEAD
   it("TCF-Pages 11: Muestra el título 'Mi Perfil'", () => {
+=======
+  //Título principal
+  it("TCF-Pages 12: Muestra el título 'Mi Perfil'", () => {
+>>>>>>> 5a8a75b4c6191aa203490aef75689025386956b5
     render(<Configuracion />);
     expect(screen.getByText(/mi perfil/i)).toBeInTheDocument();
   });
 
+<<<<<<< HEAD
   it("TCF-Pages 12: Muestra labels Nombres, Apellidos, Correo electrónico y Dirección", () => {
+=======
+  //Labels principales visibles
+  it("TCF-Pages 13: Muestra labels Nombres, Apellidos, Correo electrónico y Dirección", () => {
+>>>>>>> 5a8a75b4c6191aa203490aef75689025386956b5
     render(<Configuracion />);
     expect(screen.getByText(/nombres/i)).toBeInTheDocument();
     expect(screen.getByText(/apellidos/i)).toBeInTheDocument();
@@ -18,7 +28,12 @@ describe("Configuracion (básico)", () => {
     expect(screen.getByText(/dirección/i)).toBeInTheDocument();
   });
 
+<<<<<<< HEAD
   it("TCF-Pages 13: Los campos de texto están en readonly inicialmente", () => {
+=======
+  //Inputs de texto en modo solo-lectura inicialmente
+  it("TCF-Pages 14: Los campos de texto están en readonly inicialmente", () => {
+>>>>>>> 5a8a75b4c6191aa203490aef75689025386956b5
     render(<Configuracion />);
     const textboxes = screen.getAllByRole("textbox"); 
     expect(textboxes.length).toBeGreaterThanOrEqual(4);
@@ -27,7 +42,12 @@ describe("Configuracion (básico)", () => {
     });
   });
 
+<<<<<<< HEAD
 it("TCF-Pages 14: Los selects de Región y Comuna están deshabilitados", () => {
+=======
+  //Selects de Región y Comuna deshabilitados
+it("TCF-Pages 15: Los selects de Región y Comuna están deshabilitados", () => {
+>>>>>>> 5a8a75b4c6191aa203490aef75689025386956b5
   render(<Configuracion />);
 
   const combos = screen.getAllByRole("combobox");
@@ -40,7 +60,12 @@ it("TCF-Pages 14: Los selects de Región y Comuna están deshabilitados", () => 
 });
 
 
+<<<<<<< HEAD
   it("TCF-Pages 15: Existe el botón 'Editar' y está habilitado", () => {
+=======
+  //Botón 'Editar' visible (tipo button)
+  it("TCF-Pages 16: Existe el botón 'Editar' y está habilitado", () => {
+>>>>>>> 5a8a75b4c6191aa203490aef75689025386956b5
     render(<Configuracion />);
     const btn = screen.getByRole("button", { name: /editar/i });
     expect(btn).toBeInTheDocument();
